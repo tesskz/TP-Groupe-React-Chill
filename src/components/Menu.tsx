b290380d5ec73ade2315c649a3e6fae7bd2e9ae8
Ranya.aitgaghou@ynov.com <ranya.aitgaghou@ynov.com>
2026-09-23 14:24:32 +0200

    feat: ajouter connexion profil et déconnexion

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,7 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import "./Menu.css";
+import { clearLoggedUser } from "../store/reducers/auth";
 
 function Menu() {
+  const loggedUser = useSelector((state: any) => state.auth.loggedUser);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    dispatch(clearLoggedUser());
+    navigate("/login");
+  };
+
   return (
     <nav className="header">
       <div className="header-content">
@@ -10,9 +22,21 @@ function Menu() {
         </Link>
 
         <ul className="nav-links">
-          <li>
-            <Link to="/login">Connexion</Link>
-          </li>
+          {loggedUser && (
+            <>
+              <li>{loggedUser.firstName}</li>
+              <li>
+                <button onClick={handleLogout}>Déconnexion</button>
+              </li>
+            </>
+          )}
+
+          {!loggedUser && (
+            <li>
+              <Link to="/login">Connexion</Link>
+            </li>
+          )}
+
           <li>
             <Link to="/users">Annuaire</Link>
           </li>
